296dd1d92fba323a29fbc1cccc06cda9c3803089
Leonardo Melo <hello@leomeloxp.net>
2018-09-18 20:46:05 +0100

    Fixed misnamed prop in frontend meta tag

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
             name='viewport'
             content='width=device-width, initial-scale=1.0'
           />
-          <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+          <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
           <title>My page</title>
           <link
             rel='stylesheet'
