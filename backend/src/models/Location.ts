1bcaab97da0d4960c13fdf9dfcd07cc5b2b976f9
ibot.autom <ibot.automacoes@gmail.com>
2018-09-19 22:10:22 +0100

    On branch feature/input-location 	backend: models/Location has been created.

---
+++ backend/src/models/Location.ts
@@ -0,0 +1,59 @@
+import mongoose, { Model } from 'mongoose';
+import { ICustomDocument } from './index';
+
+export interface ILocation {
+  // System fields
+  created: Date | number;
+  updated: Date | number;
+  id: string;
+  // Custom fields
+  name: string;
+  address: string;
+  notes: string;
+  latitude: string;
+  longitude: string;
+}
+
+export interface ILocationDocument extends ILocation, ICustomDocument {}
+
+const LocationSchema = new mongoose.Schema({
+  address: {
+    required: false,
+    type: String
+  },
+  created: {
+    default: Date.now,
+    type: Date
+  },
+  latitude: {
+    required: false,
+    type: String
+  },
+  longitude: {
+    required: false,
+    type: String
+  },
+  name: {
+    required: true,
+    type: String
+  },
+  notes: {
+    required: false,
+    type: String
+  },
+  updated: {
+    default: Date.now,
+    type: Date
+  }
+});
+
+LocationSchema.pre<ILocationDocument>('save', function preSave(next): void {
+  if (this.isModified()) {
+    this.updated = Date.now();
+  }
+  next();
+});
+
+const Location: Model<ILocationDocument> = mongoose.model('Location', LocationSchema);
+
+export { Location };
