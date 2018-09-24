6d0dc8e8336a83eb40ae142942600f106db07ce6
Leonardo Melo <hello@leomeloxp.net>
2018-09-24 08:25:40 +0100

    Update backend test configuration
    babel-jest did not work for the backend so I shifted to using ts-jest instead

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,15 +3,14 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
 module.exports = {
   testRegex: TEST_REGEX,
   transform: {
-    '^.+\\.tsx?$': 'babel-jest'
+    '^.+\\.tsx?$': 'ts-jest'
   },
   testPathIgnorePatterns: [
     '<rootDir>/build/',
     '<rootDir>/node_modules/',
-    '<rootDir>/src/__test__/',
     '<rootDir>/coverage/'
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverageFrom: ['<rootDir>/src/**/*', '!**/__tests__/**/*'],
+  collectCoverageFrom: ['<rootDir>/src/**/*', '!*.test.ts'],
   collectCoverage: false
 };
